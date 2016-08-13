8817526b6e8e53be67c363db98ca92deaeeef6b1
takahashimasaya00 <takahashimasaya00@gmail.com>
2016-08-13 22:17:17 +0900

    test

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularSampleApp
+ * @description
+ * # angularSampleApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularSampleApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource'
+  ]);
