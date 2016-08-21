66b5a1198d6d7a1919549320434e45d8d703bf55
takahashimasaya00 <takahashimasaya00@gmail.com>
2016-08-22 00:08:15 +0900

    sample add

---
+++ WebContent/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularSampleApp
+ * @description
+ * # angularSampleApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularSampleApp', [
+    'ngAnimate'
+  ]);
---
+++ WebContent/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularSampleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularSampleApp
+ */
+angular.module('angularSampleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
